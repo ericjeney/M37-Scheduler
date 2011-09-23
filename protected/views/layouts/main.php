c370eee519b4df7f7a5567d4356eeadf5bbd3bc7
Eric Jeney <emjeney@gmail.com>
2011-09-23 17:48:37 -0400

    Remove date() function. It was throwing an error.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -45,7 +45,7 @@
 	<?php echo $content; ?>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
