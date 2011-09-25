3d03f8bcf4fd585c0eed2232c97686f39f2fb286
Eric Jeney <emjeney@gmail.com>
2011-09-24 21:07:15 -0400

    Change application name.

--- src/protected/config/main.php
+++ src/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'M-37 Scheduler',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
