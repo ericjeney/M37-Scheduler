56da3b6976873882682e35f5ccef2f40c3561303
Eric Jeney <emjeney@gmail.com>
2011-09-25 15:52:10 -0400

    Fix comment.

--- src/protected/components/UserIdentity.php
+++ src/protected/components/UserIdentity.php
@@ -10,12 +10,7 @@ class UserIdentity extends CUserIdentity
 	private $_id;
 	
 	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
+	 * Authenticates against the tbl_user table.
 	 */
 	public function authenticate()
 	{
