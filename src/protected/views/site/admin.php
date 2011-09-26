3993806d0a1d716c77aa0fd47bb379ce7c6044bc
Eric Jeney <emjeney@gmail.com>
2011-09-26 17:15:26 -0400

    Move some header text, and add the admin page.

---
+++ src/protected/views/site/admin.php
@@ -0,0 +1,5 @@
+<?php $this->pageTitle=Yii::app()->name . ' - Admin Area'; ?>
+
+<h1>You are currently registered as an Administrator!</h1>
+
+<p>However, Eric and Shreyas have not gotten around to coding this part yet.  Don't worry, we will!  We promise!</p>
