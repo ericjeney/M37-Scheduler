9d04f2618a38add24bb340109657ef63526b87c9
Eric Jeney <emjeney@gmail.com>
2011-09-26 20:11:28 -0400

    Center the form.

--- src/protected/views/site/picker.php
+++ src/protected/views/site/picker.php
@@ -1,3 +1,9 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Select Activity';
+?>
+
+<h1 class="center">Select an Activity</h1>
+
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -6,15 +12,17 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'offering'); ?>
-		<?php echo $form->dropDownList($model,'offering', CHtml::listData(Offering::model()->findAll(), 'id', 'title'), array("prompt"=>"Please Select an Activity")); ?>
-		<?php echo $form->error($model,'offering'); ?>
-	</div>
-
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
+	<div align="center">
+		<div class="row">
+			<?php echo $form->labelEx($model,'offering'); ?>
+			<?php echo $form->dropDownList($model,'offering', CHtml::listData(Offering::model()->findAll(), 'id', 'title'), array("prompt"=>"Please Select an Activity")); ?>
+			<?php echo $form->error($model,'offering'); ?>
+		</div>
+	
+	
+		<div class="row buttons">
+			<?php echo CHtml::submitButton('Submit'); ?>
+		</div>
 	</div>
 
 <?php $this->endWidget(); ?>
